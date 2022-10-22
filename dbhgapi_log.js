43fe91ddf60745e11880960f5eba1c646bdfb18c
Y_ALANG <92966060+yalwolf@users.noreply.github.com>
2022-10-22 11:46:44 +0800

    Update dbhgapi_log.js

--- dbhgapi_log.js
+++ dbhgapi_log.js
@@ -1,4 +1,4 @@
-window.version = 1.0.1
+window.version = "1.0.1"
 console.log("%c蛋白后宫API%cversion - " + window.version,
     "color:#000;background:#FFFF00;padding:3px 7px;border-top-left-radius: 3px;border-bottom-left-radius: 3px;",
     "color:#000;background:#FF9900;padding:3px 7px;border-top-right-radius: 3px;border-bottom-right-radius: 3px;"
