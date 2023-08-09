13e241c25eb4aedcbc283d89ec724bea463e95c3
一只屑阿狼 <92966060+yalwolf@users.noreply.github.com>
2023-08-10 04:51:20 +0800

    Create dbhgapi_log_old.js

---
+++ dbhgapi_log_old.js
@@ -0,0 +1,5 @@
+var version = "1.0.1"
+console.log("%c蛋白后宫API%cversion - " + version,
+    "color:#000;background:#FFFF00;padding:3px 7px;border-top-left-radius: 3px;border-bottom-left-radius: 3px;",
+    "color:#000;background:#FF9900;padding:3px 7px;border-top-right-radius: 3px;border-bottom-right-radius: 3px;"
+)
